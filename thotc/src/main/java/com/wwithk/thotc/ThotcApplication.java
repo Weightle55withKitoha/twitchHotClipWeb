b2ef8b48c673f0ca309425dbf865ee3bd11329b6
weightle55 <metalicboots@gmail.com>
2020-04-08 15:35:01 +0900

    weightle55 : Spring skeleton init

---
+++ thotc/src/main/java/com/wwithk/thotc/ThotcApplication.java
@@ -0,0 +1,13 @@
+package com.wwithk.thotc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThotcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThotcApplication.class, args);
+	}
+
+}
