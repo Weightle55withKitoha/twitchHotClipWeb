9b443b6f0512102531500e4d75ba7cea6fac69b9
weightle55 <metalicboots@gmail.com>
2020-05-03 21:53:50 +0900

    weightle55 : Router Config 수정

--- thotc_client/src/styles/layout/ContentsLayout.js
+++ thotc_client/src/styles/layout/ContentsLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import Test from '../../components/Test';
 
 import { BrowserRouter, Route } from "react-router-dom";
 import { RouterConfigs } from "../../configs/RouterConfigs";
