e2b6d54ed8d8a2c82cb35a36fd8e049702078b28
kitoha <kitoha@daum.net>
2020-05-04 18:39:28 +0900

    modified frontend

--- thotc_client/src/components/tables/TableToolbar.js
+++ thotc_client/src/components/tables/TableToolbar.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Toolbar, Typography } from "@material-ui/core";
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme=>(
-  {
-    bolder : {
-      fontWeight : 'bolder',
-    }
-  }
-));
+const useStyles = makeStyles((theme) => ({
+  bolder: {
+    fontWeight: "bolder",
+  },
+}));
 
 const TableToolbar = () => {
   const classes = useStyles();
   return (
     <Toolbar>
-      <Typography><h3 className={classes.bolder}>스트리머 순위</h3></Typography>
+      <Typography variant="h5" className={classes.bolder}>
+        스트리머 순위
+      </Typography>
     </Toolbar>
   );
 };
