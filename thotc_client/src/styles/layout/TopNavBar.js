6cf69c5502fa0338822c9d8c8e61a191c173c203
kitoha <kitoha@daum.net>
2020-05-04 18:39:28 +0900

    modified frontend

--- thotc_client/src/styles/layout/TopNavBar.js
+++ thotc_client/src/styles/layout/TopNavBar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import { NavbarItems } from '../../configs/NavbarItemConfigs';
-import {Link} from 'react-router-dom';
+import { NavbarItems } from "../../configs/NavbarItemConfigs";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,24 +10,24 @@ const useStyles = makeStyles((theme) => ({
   },
 
   toolbar: {
-    justifyContent : 'center',
+    justifyContent: "center",
   },
-  contents : {
-      justifyContent : 'flex-start',
-      width : '100%',
-      maxWidth : theme.contentsMaxwidth,
-      // backgroundColor : '#756650',
+  contents: {
+    justifyContent: "flex-start",
+    width: "100%",
+    maxWidth: theme.contentsMaxwidth,
+    // backgroundColor : '#756650',
   },
   title: {
     // backgroundColor : '#5f7a82',
-    margin : theme.spacing(3)
+    margin: theme.spacing(3),
   },
-  mainLogo : {
-    margin : theme.spacing(3)
+  mainLogo: {
+    margin: theme.spacing(3),
   },
-  linkNone : {
-    textDecoration : 'none',
-  }
+  linkNone: {
+    textDecoration: "none",
+  },
 }));
 
 const TopNavBar = () => {
@@ -35,19 +35,17 @@ const TopNavBar = () => {
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-          <Toolbar className={classes.contents}>
-              <Typography variant ="h5" className={classes.mainLogo}>
-                  MainLogo
+        <Toolbar className={classes.contents}>
+          <Typography variant="h5" className={classes.mainLogo}>
+            MainLogo
+          </Typography>
+          {NavbarItems.map((item, index) => (
+            <Link to={item.linkto} className={classes.linkNone} key={index}>
+              <Typography variant={item.TypoVariant} className={classes.title}>
+                {item.menuText}
               </Typography>
-          {
-              NavbarItems.map((item,index) => (
-                <Link to={item.linkto} className={classes.linkNone} >
-                  <Typography variant={item.TypoVariant} className={classes.title} key={index}>
-                      {item.menuText}
-                  </Typography>
-                </Link>
-              ))
-          }
+            </Link>
+          ))}
         </Toolbar>
       </Toolbar>
     </AppBar>
