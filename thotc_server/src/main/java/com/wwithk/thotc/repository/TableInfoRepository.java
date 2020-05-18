8d2e3858986169f8ee438b4563b85737b49995ad
kitoha <kitoha@daum.net>
2020-05-18 22:53:46 +0900

    exception features

--- thotc_server/src/main/java/com/wwithk/thotc/repository/TableInfoRepository.java
+++ thotc_server/src/main/java/com/wwithk/thotc/repository/TableInfoRepository.java
@@ -2,11 +2,15 @@ package com.wwithk.thotc.repository;
 
 import com.wwithk.thotc.dao.TableInfoDao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TableInfoRepository extends JpaRepository<TableInfoDao,Long> {
     Optional<TableInfoDao> findBystreamerName(String name);
+    @Query("SELECT * FROM TableInfoDao t ORDER BY t.viewerCount DESC")
+    List<TableInfoDao> findAllDesc();
 }
